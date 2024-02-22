742933accf30bf0860aaf4e589d524a5590c3ae8
raihua <raihua.dev@gmail.com>
2024-02-22 22:48:10 +1030

    Add profile information to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "../styles/Home.css";
+
 const Home = () => {
     return (
-        <>
-        <h1>Home</h1>
-        <p>We are on home!</p>
-        <Outlet />
-        </>
+        <div className="container">
+            <img className="profilePic" src="src/assets/peter-large.jpeg" alt="Peter Large" />
+            <article>
+                <h1>Peter Trinh</h1>
+                <p>Software Engineer Student</p>
+                <p>Hi there! Its great to have you as my visitor.</p>
+
+            </article>
+            <Outlet />
+        </div>
     )
 }
 
