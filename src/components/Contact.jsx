b744841511a04b379cd7a33ee80cc92c1ab22a26
Peter Trinh <55374648+raihua@users.noreply.github.com>
2024-03-08 10:28:31 +0000

    Add contact information to contacts component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,12 @@ const Contact = () => {
     return (
         <>
             <h1>My Contacts</h1>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/raihuadev">Linkedin</a></li>
+                <li><a href="https://github.com/raihua">Github</a></li>
+                <li><p>+61 0434861721</p></li>
+                <li><p>raihua.dev@gmail.com</p><a href="mailto:raihua.dev@gmail.com">Send email</a></li>
+            </ul>
         </>
     )
 }
