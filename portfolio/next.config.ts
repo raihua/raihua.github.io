463555a9c484c45aed64f76d5aa9a2cf3eb5a781
raihua <raihua.dev@gmail.com>
2026-07-22 00:46:08 +0930

    Add export option to output in config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+  // Optional: highly recommended for static hosting platforms to keep URLs clean
+  trailingSlash: true, 
 };
 
 export default nextConfig;
