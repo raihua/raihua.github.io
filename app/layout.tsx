f8a77eda1e546b31e29cc45324e21287d60eb2a0
raihua <raihua.dev@gmail.com>
2026-08-07 01:05:56 +0930

    Delete body padding and margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en">
-      <body>
+      <body className="p-0 m-0">
         <NavBar />
         <div className="justify-items-center p-2">
           {children}
