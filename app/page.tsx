534ee558e90d07b096b7e73603ea8174342505d4
raihua <raihua.dev@gmail.com>
2026-08-06 20:30:44 +0930

    Add homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-
+      <h1>Hi, I'm Peter Trinh</h1>
+      <p>I'm a software engineer student</p>
+      <button className="border p-2 rounded-xl m-2">Get Resume</button>
+      <button className="border p-2 rounded-xl m-2">Contact Me</button>
     </div>
   );
 }
