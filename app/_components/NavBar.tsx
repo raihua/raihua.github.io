9b34a2530d341d3be102bc82dfda2557ade854d7
raihua <raihua.dev@gmail.com>
2026-08-07 01:06:13 +0930

    Add blue bg to NavBar

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -10,10 +10,10 @@ const navLinks = [
 
 export default function NavBar() {
   return (
-    <nav className="border justify-items-center">
+    <nav className="justify-items-center bg-blue-500 text-white">
       <ul className="flex flex-row">
         {navLinks.map((link) => (
-          <li key={link.href} className="m-2 p-2 border">
+          <li key={link.href} className="p-4 hover:bg-blue-700 hover:underline">
             <Link href={link.href}>{link.label}</Link>
           </li>
         ))}
