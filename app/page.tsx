807c13a7ea1353a2aba699ef15c74bb525ae0f08
raihua <raihua.dev@gmail.com>
2026-08-07 02:54:50 +0930

    Add homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hi, I'm Peter Trinh</h1>
-      <p>I'm a software engineer student</p>
-      <button className="border p-2 rounded-xl m-2">Get Resume</button>
-      <button className="border p-2 rounded-xl m-2">Contact Me</button>
-    </div>
+    <div className="w-auto">
+      <div className="flex flex-row items-center gap-8">
+        <div>
+          <Image src="/Me.jpeg" width="400" height="400" alt="Peter Trinh" className="rounded-xl" />
+        </div>
+        <div>
+          <h1 className="text-3xl">Hi, I'm Peter Trinh</h1>
+          <p className="text-lg text-wrap">I'm a final year software engineering student and freelancer who loves solving problems with software. I love being transparent and upfront about what each solution requires and producing something we can both be proud of is my passion.</p>
+        </div>
+      </div>
+      <div className="flex gap-3">
+        <button className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded-lg text-white">Get Resume</button>
+        <button className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded-lg text-white">Contact Me</button>
+      </div >
+    </div >
   );
 }
