41865807ab6386efb41936ee447c91244a19dea6
raihua <raihua.dev@gmail.com>
2026-08-07 03:00:54 +0930

    Add Shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "./_components/NavBar";
+import { Roboto } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Peter Trinh",
@@ -10,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", roboto.variable)}>
       <body className="p-0 m-0">
         <NavBar />
         {children}
